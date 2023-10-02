7ad4c353cd3d8b3cc20062d70a0a78a2b00fd30c
R.Krishna Teja <74000968+Krishna-coder12@users.noreply.github.com>
2023-10-02 11:51:35 -0400

    Commit Changes

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -11,7 +11,14 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div className="container Header"><div className="header">Web Applications</div><div className="gradient"></div></div>
+            <div className="container Header"><div className="header">Gorup 5 Web App</div>
+                <div className="gradient"></div>
+        <div className="button">
+            <button type="button"><a href="getting-started.html">getting-started.html</a></button>
+            <button type="button"><a href="p2.html">p2.html</a></button>
+            <button type="button"><a href="p4.html">p4.html</a></button>
+            <button type="button"><a href="p5.html">p5.html</a></button>
+        </div></div>
         );
     }
 }
